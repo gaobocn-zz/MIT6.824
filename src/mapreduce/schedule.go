aeda7f37d7d41742984b55ebaaabd5175049d090
Bo Gao <bg447@cornell.edu>
2017-01-22 02:10:12 +0000

    using WaitGroup as a solution for unmutexed list

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -3,9 +3,12 @@ package mapreduce
 import (
     "fmt"
     "container/list"
+    "sync"
 )
 
-var response chan int
+var responise chan int
+
+var wg sync.WaitGroup
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -39,35 +42,40 @@ func (mr *Master) schedule(phase jobPhase) {
 
     nfinished := 0
     // this doesn't work, my guess is taskList is not mutexed, so it may not be up-to-date
-    //for nfinished < ntasks {
-        //fmt.Println(nfinished, ntasks, taskList.Len())
-        for taskList.Len() > 0 {
+    for nfinished < ntasks {
+        fmt.Println(nfinished, ntasks, taskList.Len())
+        if taskList.Len() > 0 {
             select {
             case wname := <-mr.registerChannel:
                 itask = taskList.Remove(taskList.Front()).(int)
                 doTaskArgs := &DoTaskArgs{JobName: mr.jobName, File: mr.files[itask],
                     Phase: phase, TaskNumber: itask, NumOtherPhase: nios}
                 //fmt.Println("map task: ", itask)
+                wg.Add(1)
                 go func() { // in fact main can exit before this, so we should probabily use waitgroup
                     ok := call(wname, "Worker.DoTask", doTaskArgs, new(struct{}))
                     if ok {
                         nfinished++
-                        //mr.registerChannel <- wname
                         mr.Register(&RegisterArgs{wname}, new(struct{}))
                     } else {
                         taskList.PushFront(itask)
                     }
+                    wg.Done()
                 }()
             }
+        } else {
+            fmt.Println("waiting")
+            wg.Wait()
+            fmt.Println("waiting end")
         }
-    //}
+    }
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
 
-func HandleResponse(r int, nfinished *int, taskArr []int) {
+func HandleResponse(r int, nfinished *int, l *list.List) {
     if r < 0 {
         *nfinished++
     } else {
-        taskArr = append(taskArr, r)
+        l.PushFront(r)
     }
 }
